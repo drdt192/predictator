413a8270077cbe28dada8a045fca2d49c0b699ad
lumi192 <robertsoncruz192@gmail.com>
2026-03-08 20:34:15 +0800

    other stupid change

--- core.py
+++ core.py
@@ -5,14 +5,13 @@ from tokenizers.pre_tokenizers import Whitespace
 
 tokenizer = Tokenizer(BPE())
 tokenizer.pre_tokenizer = Whitespace()
-
 #trainer = BpeTrainer(special_tokens=["[UNK]", "[CLS]", "[SEP]", "[PAD]", "[MASK]"])
 #dataset = ["data/training_dataset.txt"]
 #tokenizer.train(dataset, trainer)
 
-tokenizer.from_file("data/tokenizer.json")
-
-output = tokenizer.encode("Hello, how are you?")
+tokenizer = tokenizer.from_file("data/tokenizer.json")
+input = input("yo give text\n")
+output = tokenizer.encode(input)
 print(output.tokens)
 
 #tokenizer.save("data/tokenizer.json")
