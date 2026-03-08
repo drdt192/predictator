fa28be3cbed0c18659482ef15f66d27ac59497ac
lumi192 <robertsoncruz192@gmail.com>
2026-03-08 20:56:04 +0800

    comment

--- core.py
+++ core.py
@@ -1,10 +1,10 @@
 from tokenizers import Tokenizer
 from tokenizers.models import BPE
 #from tokenizers.trainers import BpeTrainer
-from tokenizers.pre_tokenizers import Whitespace
+#from tokenizers.pre_tokenizers import Whitespace
 
 tokenizer = Tokenizer(BPE())
-tokenizer.pre_tokenizer = Whitespace()
+#tokenizer.pre_tokenizer = Whitespace()
 #trainer = BpeTrainer(special_tokens=["[UNK]", "[CLS]", "[SEP]", "[PAD]", "[MASK]"])
 #dataset = ["data/training_dataset.txt"]
 #tokenizer.train(dataset, trainer)
